ad190fce368fba84d4de99e22364722619a3c4d9
Samana97 <85249413+Samana97@users.noreply.github.com>
2021-06-09 12:01:50 +0530

    Update GourmetCoffeeShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -8,6 +8,10 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+        TotalBillValue{get; set;}:double
+GourmentCofeeShop(intiCode,string iName,Dictionary<int, double>iPrice)
+PayperPiece(int quantity):double
+PayperPiece(int weight):double
+TaxOnTotalBill():double
     }
 }
